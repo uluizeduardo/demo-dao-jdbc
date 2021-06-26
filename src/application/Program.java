638e14ec0d0ce535897c148e8837adad1d690f9c
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-06-26 20:18:39 -0300

    Foi implementado a classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,21 @@
 package application;
 
-import db.DB;
 import model.Department;
+import model.Seller;
 
-import java.sql.Connection;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
-        Connection conect = DB.getConnection();
-        DB.closeConnection();
+//        Connection conect = DB.getConnection();
+//        DB.closeConnection();
 
+
+        Department dp = new Department(1, "Books");
+
+        Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, dp);
+
+        System.out.println(seller);
     }
 }
